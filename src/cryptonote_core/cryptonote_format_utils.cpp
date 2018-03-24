fb2a80f780ee539d13d5c540616679510f43bd1c
MoneroOcean <support@moneroocean.stream>
2018-03-24 21:03:13 +0100

    Logic fix

--- src/cryptonote_core/cryptonote_format_utils.cpp
+++ src/cryptonote_core/cryptonote_format_utils.cpp
@@ -860,7 +860,7 @@ namespace cryptonote
   //---------------------------------------------------------------
   bool check_proof_of_work_v2(const block& bl, difficulty_type current_diffic, crypto::hash& proof_of_work)
   {
-    if (BLOCK_MAJOR_VERSION_2 != bl.major_version ||
+    if (BLOCK_MAJOR_VERSION_2 != bl.major_version &&
         BLOCK_MAJOR_VERSION_3 != bl.major_version)
       return false;
 
