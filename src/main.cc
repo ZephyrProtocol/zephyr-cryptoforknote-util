b47f6475acdca423f0532ee5a1b7e2ec25a51e32
MoneroOcean <support@moneroocean.stream>
2020-04-24 12:46:49 -0700

    Fix for v12

--- src/main.cc
+++ src/main.cc
@@ -222,7 +222,7 @@ NAN_METHOD(construct_block_blob) { // (parentBlockTemplateBuffer, nonceBuffer, c
     if (blob_type == BLOB_TYPE_CRYPTONOTE_XTNC || blob_type == BLOB_TYPE_CRYPTONOTE_CUCKOO) {
         if (info.Length() != 4) return THROW_ERROR_EXCEPTION("You must provide 4 arguments.");
         Local<Array> cycle = Local<Array>::Cast(info[3]);
-        for (int i = 0; i < 32; i++ ) b.cycle.data[i] = cycle->Get(i)->NumberValue(isolate->GetCurrentContext()).ToChecked();
+        for (int i = 0; i < 32; i++ ) b.cycle.data[i] = cycle->Get(isolate->GetCurrentContext(), i).ToLocalChecked()->NumberValue(isolate->GetCurrentContext()).ToChecked();
     }
 
     if (!block_to_blob(b, output)) return THROW_ERROR_EXCEPTION("Failed to convert block to blob");
